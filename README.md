d0c4fb992edea848df6ea854d63da962e64321ae
VishwasKumar <vishwaskumar58@gmail.com>
2016-05-28 12:18:06 -0400

    removing the resources and added content to the readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# IPLSentimentAlalysis
+# IPLTweetsCollector
 
-How to run get tweets?
+Follow the tutorial on http://iag.me/socialmedia/how-to-create-a-twitter-app-in-8-easy-steps/ to create your own twitter app and add the tokens to the get_tweets.py file.
 
-python get_tweets.py > tweets.txt
+
+    **How to run get_tweets**
+
+      python get_tweets.py > tweets.txt
+
+````
+**How to use Exporter**
+
+Get tweets by username
+    python Exporter.py --username 'barackobama' --maxtweets 1
+Get tweets by query search
+    python Exporter.py --querysearch 'europe refugees' --maxtweets 1
+Get tweets by username and bound dates
+    python Exporter.py --username 'barackobama' --since 2015-09-10 --until 2015-09-12 --maxtweets 1
--- get_tweets.py
+++ get_tweets.py
@@ -1,17 +1,14 @@
-
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-access_token = "501864645-WZ0ivAcJpujvJqo0Q9Yh4YeQtimywoJBpZIqQL0O"
-access_token_secret = "LLQFJMCtB7ZWIZR621aDeXOicInaMk31oEphT90mSMmsY"
-consumer_key = "Ku7IdkkLxzyePUMamqpCeCwgY"
-consumer_secret = "g0iCdW5ShVnuzaokFC4QEm7jjyMb6NX2723qXpcjbL3y4uozzi"
-
+access_token = "YOUR ACCESS TOKEN"
+access_token_secret = "YOUR ACCESS TOKEN SECRET"
+consumer_key = "YOUR CONSUMER TOKEN"
+consumer_secret = "YOUR CONSUMER SECRET"
 
 
 class StdOutListener(StreamListener):
-
     def on_data(self, data):
         print data
         return True
@@ -21,12 +18,11 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
-
-    #This handles Twitter authetification and the connection to Twitter Streaming API
+    # This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
     stream.filter(track=['#ipl2016'])
